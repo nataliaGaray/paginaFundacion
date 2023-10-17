ebaf43c07258bbc30431a34886c3ca356b289e45
Natalia <garaynatalia@yahoo.com.ar>
2023-10-17 19:45:19 -0300

    api y me olvide contraseña

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+ // ANIMAR EL PARRAFO
+ document.addEventListener("DOMContentLoaded", function() {
+    
+    const parrafo = document.querySelector(".parrafoH1");
+  
+   
+    parrafo.classList.add("animado");
+  }); // Espera a que el contenido de la página esté completamente cargado.
+window.addEventListener("load", function() {
+    // Selecciona el elemento h1.
+    const h1Element = document.querySelector(".h1Paginas");
 
- // inicioSesion
+    // Inicialmente, establece la opacidad en 0 para ocultar el elemento.
+    h1Element.style.opacity = 0;
+
+    // Realiza la animación de desvanecimiento (fade-in).
+    let opacity = 0;
+    const animationInterval = setInterval(function() {
+        if (opacity >= 1) {
+            clearInterval(animationInterval); // Detén la animación cuando la opacidad alcanza 1.
+        } else {
+            opacity += 0.02; // Incrementa la opacidad gradualmente.
+            h1Element.style.opacity = opacity;
+        }
+    }, 20); // Cambia el valor de 20 para ajustar la velocidad de la animación.
+});
+
+// inicioSesion
 document.getElementById("login-form").addEventListener("submit", function(e) {
     e.preventDefault();
     
@@ -22,8 +48,6 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
     
     message.style.display = "block";
 });
-
-
 // TACHAR LOS NO
 
 // Espera a que la página se cargue completamente
@@ -74,3 +98,27 @@ window.addEventListener("load", function() {
     }, 20); // Cambia el valor de 20 para ajustar la velocidad de la animación.
 });
 
+document.getElementById("forgot-password-button").addEventListener("click", function () {
+    // Pedir al usuario su dirección de correo electrónico
+    const email = prompt("Por favor, ingrese su dirección de correo electrónico:");
+  
+    if (!email) {
+      return; // El usuario canceló la operación
+    }
+  
+    // Recuperar la base de datos de usuarios almacenada en el localStorage
+    const database = JSON.parse(localStorage.getItem("database"));
+  
+    // Buscar la usuaria en la base de datos que coincida con el correo electrónico ingresado
+    const user = database.find(usuario => usuario.email === email);
+  
+    if (user) {
+      // Si se encontró la usuaria, mostrar sus datos
+    alert(`Sus datos de registro son:\nNombre: ${user.nombre}\nUsername: ${user.username} \nPassword ${user.password}`);
+    } else {
+      // Si no se encontró la usuaria, mostrar un mensaje de error
+      alert("No se encontraron datos de registro para el correo electrónico proporcionado.");
+    }
+  });
+  
+  
