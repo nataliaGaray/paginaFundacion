a4814500af329ce0d59e15635b4b8ec66db8003a
Natalia <garaynatalia@yahoo.com.ar>
2023-10-17 11:36:34 -0300

    usuaria validado, nuevo js de registro

--- script.js
+++ script.js
@@ -1,121 +1,28 @@
-// CAFECITO
-document.addEventListener("DOMContentLoaded", function() {
-    const buttonCafecito = document.getElementById("cafecito");
 
-    buttonCafecito.addEventListener("click", function() {
-        // Crea un objeto de preferencia con los detalles del producto
-        const preference = {
-            items: [
-                {
-                    title: "Cafecito",
-                    quantity: 1,
-                    currency_id: "ARS", // Puedes cambiar la moneda según tu país
-                    unit_price: 100 // El precio del café en centavos
-                }
-            ]
-        };
-
-        // Reemplaza con tu token de acceso de Mercado Pago
-        const accessToken = "APP_USR-8069221756896494-101418-644c13cf04749a71a52a958eb13e8b68-59122948";
-
-        // Realiza una llamada a la API de Mercado Pago para obtener la URL de pago
-        
-        curl 'https://api.mercadopago.com.test', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${accessToken}`
-            },
-            body: JSON.stringify(preference)
-        }
-        .then(response => response.json())
-        .then(data => {
-            // Redirige al usuario a la página de pago de Mercado Pago
-            window.location.href = data.init_point;
-        })
-        .catch(error => {
-            // Maneja errores aquí
-            alert("Hubo un error al procesar el pago.");
-        });
-    });
-});
-
-// index
-// document.addEventListener("DOMContentLoaded", function() {
-//     const loginForm = document.getElementById("login-form");
-//     const bienvenidaMensaje = document.getElementById("bienvenidaMensaje");
-
-//     loginForm.addEventListener("submit", function(e) {
-//         e.preventDefault();
-
-//         const username = document.getElementById("new-username").value;
-//         const password = document.getElementById("new-password").value;
-
-        // Enviar la solicitud al servidor para verificar la autenticación
-        fetch('verificar_usuario.php', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.valid) {
-                bienvenidaMensaje.textContent = "Bienvenidas a Patagónicas en Tecnología, ${data.username}";
-            } else {
-                bienvenidaMensaje.textContent = "Por favor, regístrate para iniciar sesión.";
-            }
-        });
-        console.log();
-//     });
-// });
-
-// registro
-document.addEventListener("DOMContentLoaded", function() {
-    const registroForm = document.getElementById("registro-form");
-    const bienvenidaMensaje = document.getElementById("bienvenidaMensaje");
-
-    registroForm.addEventListener("submit", function(e) {
-        e.preventDefault();
-
-        const nombre = document.querySelector('input[placeholder="Nombre"]').value;
-        const email = document.querySelector('input[placeholder="email"]').value;
-        const nombreUsuario = document.getElementById("crearUserName").value;
-        const contraseña = document.getElementById("login-password").value;
-
-        // Enviar los datos de registro al servidor
-        fetch('registrar_usuario.php', {
-            method: 'POST',
-            body: JSON.stringify({ nombre, email, nombreUsuario, contraseña }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.valid) {
-                bienvenidaMensaje.textContent = `¡Bienvenidas a Patagónicas en Tecnología, ${nombreUsuario}!`;
-            } else {
-                bienvenidaMensaje.textContent = "No se pudo completar el registro. Inténtalo de nuevo.";
-            }
-        });
-    });
-});
-
-
-  
-
-
-  // ANIMAR EL PARRAFO
-  document.addEventListener("DOMContentLoaded", function() {
+ // inicioSesion
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    e.preventDefault();
     
-    const parrafo = document.querySelector(".parrafoH1");
-  
-   
-    parrafo.classList.add("animado");
-  });
-  
+    const username = document.getElementById("new-username").value; // Cambiado de crearUserName a new-username
+    const password = document.getElementById("new-password").value;
+    const message = document.getElementById("message");
+    
+    // Obtener la base de datos simulada (debe coincidir con la de Registro.html)
+    const database = JSON.parse(localStorage.getItem("database")) || [];
+    
+    const user = database.find(user => user.username === username && user.password === password);
+    
+    if (user) {
+        message.innerText = "¡Bienvenida, " + user.nombre + "!"; // Agregado el nombre de la usuaria
+        message.style.color = "blanco";
+    } else {
+        message.innerText = "Por favor, regístrate.";
+        message.style.color = "blanco";
+    }
+    
+    message.style.display = "block";
+});
+
 
 // TACHAR LOS NO
 
@@ -140,8 +47,30 @@ document.addEventListener("DOMContentLoaded", function() {
     tacharElementos(0);
   });
  
+ // ANIMAR EL PARRAFO
+ document.addEventListener("DOMContentLoaded", function() {
+    
+    const parrafo = document.querySelector(".parrafoH1");
+  
+   
+    parrafo.classList.add("animado");
+  }); // Espera a que el contenido de la página esté completamente cargado.
+window.addEventListener("load", function() {
+    // Selecciona el elemento h1.
+    const h1Element = document.querySelector(".h1Paginas");
 
+    // Inicialmente, establece la opacidad en 0 para ocultar el elemento.
+    h1Element.style.opacity = 0;
 
-
-
+    // Realiza la animación de desvanecimiento (fade-in).
+    let opacity = 0;
+    const animationInterval = setInterval(function() {
+        if (opacity >= 1) {
+            clearInterval(animationInterval); // Detén la animación cuando la opacidad alcanza 1.
+        } else {
+            opacity += 0.02; // Incrementa la opacidad gradualmente.
+            h1Element.style.opacity = opacity;
+        }
+    }, 20); // Cambia el valor de 20 para ajustar la velocidad de la animación.
+});
 
